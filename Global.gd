44d97160d512d027719a792f3ff82c629ff51237
João A. Marson <jamarson@gmail.com>
2021-09-05 13:42:06 -0300

    Add borders

--- Global.gd
+++ Global.gd
@@ -1,3 +1,7 @@
 extends Node
 
 var score = 0
+var screen_width = 1024
+const border_width = 10
+var left_border_x = screen_width/3
+var right_border_x = (screen_width/3) * 2
--- MainRoot.gd
+++ MainRoot.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var Border = load("res://Borders.tscn")
+onready var Global = get_node("/root/Global")
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -9,13 +11,16 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
+	add_bounds()
 	enemy_factory()
 	
-
-var Enemy = load("res://Enemy.tscn")
-var timer = null
-var enemy_delay = 1
-var row_width = 60
+func add_bounds():
+	var border_left = Border.instance()
+	var border_right = Border.instance()
+	border_left.position = Vector2(Global.left_border_x-Global.border_width,0)
+	border_right.position = Vector2(Global.right_border_x,0)
+	add_child(border_left)
+	add_child(border_right)
 
 func enemy_factory():
 	timer = Timer.new()
